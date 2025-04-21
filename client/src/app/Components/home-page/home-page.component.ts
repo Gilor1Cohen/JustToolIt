8819df22563b2a39d0f9a38b7dbd6e61d2c00a93
Gilor Cohen <gilor6811@gmail.com>
2025-04-21 13:38:44 +0300

    Added more of css styles

--- client/src/app/Components/home-page/home-page.component.ts
+++ client/src/app/Components/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
